a9afe6145cd2719aac10e0cbf3207709b21b2691
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-26 12:43:18 -0600

    Drop address info from users and create user addresses table

---
+++ db/migrate/20190726182015_create_user_addresses.rb
@@ -0,0 +1,12 @@
+class CreateUserAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_addresses do |t|
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.string :alias
+      t.references :user, foreign_key: true
+    end
+  end
+end
