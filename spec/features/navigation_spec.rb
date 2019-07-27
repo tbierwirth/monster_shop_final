3a68ea8fa6dca23cd8896bf06184b6c0c93f93f5
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-27 14:09:38 -0600

    Remove address info from user and add user_addresses creation

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -160,7 +160,8 @@ RSpec.describe 'Site Navigation' do
   describe 'As a Merchant User' do
     before :each do
       @merchant = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @m_user = @merchant.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      @m_user = @merchant.users.create(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
+      @m_user.user_addresses.create(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, alias: 'Home')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
     end
 
@@ -261,7 +262,8 @@ RSpec.describe 'Site Navigation' do
 
   describe 'As an Admin' do
     before :each do
-      @admin = User.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: :admin)
+      @admin = User.create(name: 'Megan', email: 'megan@example.com', password: 'securepassword', role: :admin)
+      @admin.user_addresses.create(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, alias: 'Home')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
     end
 
