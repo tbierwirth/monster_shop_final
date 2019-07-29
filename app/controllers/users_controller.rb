aa7d866f562ff97fd57b97b259985afd7423777a
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-29 15:29:07 -0600

    Update user create action to save a new address to user upon registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    @address = @user.addresses.new(address_params)
+    if @user.save && @address.save
       session[:user_id] = @user.id
       flash[:notice] = "Welcome, #{@user.name}!"
       redirect_to profile_path
     else
       generate_flash(@user)
+      generate_flash(@address)
       render :new
     end
   end
@@ -44,6 +46,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :password)
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def address_params
+    params.permit(:address, :city, :state, :zip,)
   end
 end
