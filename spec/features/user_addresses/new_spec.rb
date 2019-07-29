aad087f9f7a0edad650ce2324811628e2b41d3e1
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-29 10:53:14 -0600

    Create test for adding a new address to profile

---
+++ spec/features/user_addresses/new_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "User Profile Path" do
+  describe "As a registered user" do
+    before :each do
+      @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
+      @user_address = @user.user_addresses.create(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, alias: 'Home')
+    end
+
+    it "I can add a new address to my profile" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit profile_path
+      click_on 'New Address'
+
+      expect(current_path).to eq(new_address_path)
+      within '#new-address' do
+        fill_in "Address", with: '321 Work St'
+        fill_in "City", with: 'Aurora'
+        fill_in "State", with: 'CO'
+        fill_in "Zip", with: '80012'
+        fill_in "Alias", with: 'Work'
+
+        click_on 'Create Address'
+      end
+
+      expect(current_path).to eq(profile_path)
+      expect(page).to have_content('123 Main St')
+      expect(page).to have_content('Denver')
+      expect(page).to have_content('CO')
+      expect(page).to have_content('80218')
+      expect(page).to have_content('Home')
+
+      expect(page).to have_content('321 Work St')
+      expect(page).to have_content('Aurora')
+      expect(page).to have_content('CO')
+      expect(page).to have_content('80012')
+      expect(page).to have_content('Work')
+    end
+  end
+end
