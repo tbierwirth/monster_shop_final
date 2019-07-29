72b9d5e3188ea8bebcce222928455b16794d67d2
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-29 13:51:54 -0600

    Add address creation action

--- app/controllers/user/addresses_controller.rb
+++ app/controllers/user/addresses_controller.rb
@@ -4,6 +4,18 @@ class User::AddressesController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @address = @user.addresses.new(address_params)
+    if @address.save
+      redirect_to profile_path
+      flash[:notice] = "A new address has been added to your profile"
+    else
+      generate_flash(@address)
+    end
+  end
 
+  private
+  def address_params
+    params.require(:address).permit(:address, :city, :state, :zip, :alias)
   end
 end
