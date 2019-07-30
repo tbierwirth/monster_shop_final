f15d24a575d3dbd5ac12832772b3ad128abbb6dc
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-30 15:04:02 -0600

    Add test to expect not to see edit address link if there is a shipped order to it

--- spec/features/user/addresses/edit_spec.rb
+++ spec/features/user/addresses/edit_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe "User Profile Path" do
   describe "As a registered user" do
     before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
       @user_address = @user.addresses.create(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, nickname: 'Home')
+      @order_1 = @user.orders.create!(status: "shipped", address_id: @user_address.id)
+      @order_item_1 = @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: true)
       visit login_path
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
@@ -32,6 +36,14 @@ RSpec.describe "User Profile Path" do
         expect(page).to have_content("Aurora")
         expect(page).to have_content("80012")
         expect(page).to have_content("Work")
+      end
+    end
+
+    it 'I can not edit an address if there is a shipped order on it' do
+      visit profile_path
+
+      within "#address-#{@user_address.id}" do
+        expect(page).to_not have_link('Edit Address')
       end
     end
 
