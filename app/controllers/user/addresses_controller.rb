27d9392639f97735cdfd3e1e81b5f978326442b7
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-29 21:59:59 -0600

    Add destroy user address action

--- app/controllers/user/addresses_controller.rb
+++ app/controllers/user/addresses_controller.rb
@@ -30,6 +30,13 @@ class User::AddressesController < ApplicationController
     end
   end
 
+  def destroy
+    @address = Address.find(params[:id])
+    @address.delete
+    flash[:notice] = 'Address has been deleted.'
+    redirect_to profile_path
+  end
+
   private
   def address_params
     params.require(:address).permit(:address, :city, :state, :zip, :alias)
