ad85fae7fe8ac5b2ed102b984cfbe965f0dd9c82
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-30 14:03:11 -0600

    Add model test for getting all address nicknames for a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe User do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
   end
+
+  describe 'Class Methods' do
+    it '.address_names' do
+      user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
+      address_1 = user.addresses.create(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, nickname: 'Home')
+      address_2 = user.addresses.create(address: '321 Rocky Rd', city: 'Aurora', state: 'CO', zip: 80012, nickname: 'Work')
+
+      expect(user.address_names).to eq([address_1.nickname, address_2.nickname])
+    end
+  end
 end
