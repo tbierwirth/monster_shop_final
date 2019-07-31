9df0eda4c796489dd8fb7aab13144de0632d03f6
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-30 18:11:15 -0600

    Add model test for shipped_orders? method

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Address do
     it {should validate_presence_of :zip}
     it {should validate_presence_of :nickname}
   end
+
+  describe 'Class Mehods' do
+    it '.shipped_orders?' do
+      user = User.create!(name: 'Megan', email: 'megan_1@example.com', password: 'securepassword')
+      address_1 = user.addresses.create(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, nickname: 'Home')
+      order_1 = user.orders.create!(status: "shipped", address_id: address_1.id)
+
+      expect(address_1.shipped_orders?).to eq(true)
+    end
+  end
 end
